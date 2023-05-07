92994768e5f3762d5a3aa27311d6885e946a55b9
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-05-07 17:26:36 +0100

    feature: Filter for amenities

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -12,31 +12,31 @@ $(document).ready(function () {
   $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
     if (data.status === 'OK') { $('div#api_status').addClass('available'); } else { $('div#api_status').removeClass('available'); }
   });
-	$.ajax({
-		url: "http://0.0.0.0:5001/api/v1/places_search",
-		type: 'POST',
-		data: '{}',
-		contentType: 'application/json; charset=utf-8',
-		dataType: 'json',
-		success: function(result) {
-			$.each(result, function(index, value) {
-				$('.places').append("<article><div class = 'title'><h2>" +
-					value.name + "</h2> <div class ='price_by_night'> $" +
-					value.price_by_night + '</div> </div>' +
-					"<div class = 'information'>" +
-					"<div class='max_guest'>" +
-					"<i class='fa fa-users fa-3x' aria-hidden='true'>" +
-					'</i> <br />' + value.max_guest +
-					" Guests </div> <div class='number_rooms'>" +
-					"<i class='fa fa-bed fa-3x' aria-hidden='true'></i>" +
-					'<br />' + value.number_rooms +
-                            		" Bedrooms </div> <div class='number_bathrooms'>" +
-                            		"<i class='fa fa-bath fa-3x' aria-hidden='true'>" +
-                            		'</i> <br />' + value.number_bathrooms +
-                            		" Bathroom </div> </div> <div class='user'>" +
-                            		"</div> <div class='description'>" +
-                            		value.description + '</div> </article>');
-			});
-		}
-	});
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    type: 'POST',
+    data: '{}',
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    success: function (result) {
+      $.each(result, function (index, value) {
+        $('section.places').append("<article><div class = 'title_box'><h2>" +
+          value.name + "</h2> <div class ='price_by_night'> $" +
+          value.price_by_night + '</div> </div>' +
+          "<div class = 'information'>" +
+          "<div class='max_guest'>" +
+          "<i class='fa fa-users fa-3x' aria-hidden='true'>" +
+          '</i> <br />' + value.max_guest +
+          " Guests </div> <div class='number_rooms'>" +
+          "<i class='fa fa-bed fa-3x' aria-hidden='true'></i>" +
+          '<br />' + value.number_rooms +
+          " Bedrooms </div> <div class='number_bathrooms'>" +
+          "<i class='fa fa-bath fa-3x' aria-hidden='true'>" +
+          '</i> <br />' + value.number_bathrooms +
+          " Bathroom </div> </div> <div class='user'>" +
+          "</div> <div class='description'>" +
+          value.description + '</div> </article>');
+      });
+    }
+  });
 });
---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  const amenityList = {};
+  $('input[type=checkbox]').on('click', function () {
+    $('input:checked').each(function () {
+      amenityList[$(this).attr('data-id')] = $(this).attr('data-name');
+    });
+    $('.amenities h4').text(Object.values(amenityList).join(', '));
+    if (amenityList.length === 0) {
+      $('.amenities h4').html('&nbsp;');
+    }
+  });
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') { $('div#api_status').addClass('available'); } else { $('div#api_status').removeClass('available'); }
+  });
+  function send (filter = '{}') {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      type: 'POST',
+      data: JSON.stringify(Object.assign({}, filter)),
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      success: function (result) {
+        $('section.places').empty();
+        $.each(result, function (index, value) {
+          $('section.places').append("<article><div class = 'title_box'><h2>" +
+            value.name + "</h2> <div class ='price_by_night'> $" +
+            value.price_by_night + '</div> </div>' +
+            "<div class = 'information'>" +
+            "<div class='max_guest'>" +
+            "<i class='fa fa-users fa-3x' aria-hidden='true'>" +
+            '</i> <br />' + value.max_guest +
+            " Guests </div> <div class='number_rooms'>" +
+            "<i class='fa fa-bed fa-3x' aria-hidden='true'></i>" +
+            '<br />' + value.number_rooms +
+            " Bedrooms </div> <div class='number_bathrooms'>" +
+            "<i class='fa fa-bath fa-3x' aria-hidden='true'>" +
+            '</i> <br />' + value.number_bathrooms +
+            " Bathroom </div> </div> <div class='user'>" +
+            "</div> <div class='description'>" +
+            value.description + '</div> </article>');
+        });
+      }
+    });
+  }
+  send();
+  const filter = { amenities: amenityList };
+  $('button').on('click', function () {
+    send(filter);
+  });
+});
